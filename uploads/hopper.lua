2770d5d65b2f053e01845c11c9064b6ccd069a98
Shelvacu <shelvacu@gmail.com>
2020-07-04 18:21:45 -0700

    Add a small script to make a turtle act as a hopper

--- uploads/hopper.lua
+++ uploads/hopper.lua
@@ -13,11 +13,24 @@ end
 
 while true do
   if inventoryHasEmptySlot() then
+    --print"suck"
     turtle.suckUp()
+    sleep(0.1)
   end
   if turtle.detectDown() then
+    --print"drop"
     if not turtle.dropDown() then
+      --print"sleep"
       sleep(5)
+    end
+    if turtle.getItemCount() == 0 then
+      --print"inc"
+      local selected = turtle.getSelectedSlot()
+      if selected == 16 then
+        turtle.select(1)
+      else
+        turtle.select(selected + 1)
+      end
     end
   end
 end
